1a01a89bc0cd50b92232a107c54e2693aeaf444d
Cameron Prybol <cameron.prybol@gmail.com>
2022-08-20 22:08:05 +0000

    move to repo2docker style

--- build-command.sh
+++ build-command.sh
@@ -1,5 +1,9 @@
 # jupyter-repo2docker https://github.com/cjprybol/sars-cov2-pangenome-analysis.git \
 #     --image-name sas-cov2-pangenome-analysis-$(date +"%Y%m%d%H%M%S")
 
-jupyter-repo2docker . \
-    --image-name sas-cov2-pangenome-analysis-$(date +"%Y%m%d%H%M%S")
+jupyter-repo2docker \
+    --no-run \
+    --push \
+    --image-name sas-cov2-pangenome-analysis-$(date +"%Y%m%d%H%M%S") \
+    --target-repo-dir workspace \
+    .
